7e496868ab34504194c9df2094f8a7137e614dea
=john <=johnphpunit@gmail.com>
2018-08-02 17:15:50 +0530

    tweak on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Laravel 5
+                This is Laravel 5
                 </div>
 
                 <div class="links">
