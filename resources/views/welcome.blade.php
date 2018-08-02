b47dcff02a7456783df521c493dd480d27474228
=john <=johnphpunit@gmail.com>
2018-08-02 17:13:46 +0530

    update on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                Laravel 5
+                Welcome to Laravel 5
                 </div>
 
                 <div class="links">
